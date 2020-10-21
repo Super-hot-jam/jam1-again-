5d976492d54c774a4430edb0d0e8b82c967fd946
Joseph <josephgarrad2017@gmail.com>
2020-10-21 23:09:21 +0100

    added movement, still working on rotation

---
+++ Super-Hot-Jam/Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D Rb;
+    public float speed = 4;
+    private Vector3 origpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origpos = transform.position;
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+ 
+    void Movement()
+    {
+
+
+
+        Vector2 moveinput = new Vector2(Input.GetAxis("left_joyhori"), Input.GetAxis("left_joyverti"));
+
+        Vector2 forward = transform.up;
+        Vector2 right = transform.right;
+
+        forward.x = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 DesMoveDir = (forward * -moveinput.y + right * moveinput.x).normalized;
+
+        Rb.MovePosition( transform.position + DesMoveDir * (speed * Time.deltaTime));
+
+      
+    }
+    void Update()
+    {
+        Movement();
+        //Vector2 Look = gameObject.GetComponent<Rigidbody2D>().velocity;
+        //if (Look != Vector2.zero)
+        //{
+        //    float angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg;
+        //    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //}
+    }
+}
