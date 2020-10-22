ec75b970fbf3ee41f55315bd7f773a31d8849f16
Joseph <josephgarrad2017@gmail.com>
2020-10-22 14:41:09 +0100

    added time scaling

--- Super-Hot-Jam/Assets/PlayerMovement.cs
+++ Super-Hot-Jam/Assets/PlayerMovement.cs
@@ -8,62 +8,54 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D Rb;
     public float speed = 4;
-    private Vector3 origpos;
     float hori;
     float verti;
     Vector2 Look;
+    public TimeManager timemanager;
+    public bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
-        origpos = transform.position;
+
         Rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
- 
-   public void Movement()
-    {
-
-
-
-       Vector2 moveinput = new Vector2(Input.GetAxis("left_joyhori"), Input.GetAxis("left_joyverti"));
-
-
-
-        Vector2 forward = transform.up;
-        Vector2 right = transform.right;
-
-        forward.x = 0;
-        right.y = 0;
-
-        forward.Normalize();
-        right.Normalize();
-
-        Vector3 DesMoveDir = (forward * -moveinput.y + right * moveinput.x).normalized;
-
-        Rb.MovePosition(transform.position + DesMoveDir * (speed * Time.deltaTime));
-
-        if (DesMoveDir != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, DesMoveDir);
-        }
-    }
    void Update()
     {
         hori = Input.GetAxis("left_joyhori");
         verti = Input.GetAxis("left_joyverti");
         Look = new Vector2(hori * speed, -verti * speed);
+        slowmoving();
     }
     private void FixedUpdate()
     {
 
-        //Movement();
         Rb.velocity = Look;
         if (Look != Vector2.zero)
         {
+            isMoving = true;
             transform.rotation = Quaternion.LookRotation(Vector3.forward, Look);
         }
+        else if(Look == Vector2.zero)
+        {
+            isMoving = false;
+        }
+        
 
+    }
+    void slowmoving()
+    {
+        if(!isMoving)
+        {
+          
+             timemanager.Slowtime();
+            
+        }
+        if(isMoving)
+        {
+            timemanager.speedtime();
+        }    
+     
     }
   
 }
---
+++ Super-Hot-Jam/Assets/TimeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public float slowdownFactor = 0.05f;
+
+    public void Slowtime()
+
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+    public void speedtime()
+    {
+        Time.timeScale += (2)* Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+}
