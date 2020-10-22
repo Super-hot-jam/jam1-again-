63164aae26937f05726942a1c56daa75877a6b20
Unknown <william4.james@live.uwe.ac.uk>
2020-10-22 17:11:04 +0100

    Quick fix cos of using a 3d function
    changed sphereoverlap to circleoverlap

--- Super-Hot-Jam/Assets/Scripts_Will/PlayerAttack.cs
+++ Super-Hot-Jam/Assets/Scripts_Will/PlayerAttack.cs
@@ -8,7 +8,7 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Attack();
     }
 
     // Update is called once per frame
@@ -20,10 +20,21 @@ public class PlayerAttack : MonoBehaviour
     void Attack()
     {
         //get all colliders around player within range of the weapon reach
-        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, currentWeapon.weaponReach);
+        //Collider[] collidersInRange = Physics.OverlapSphere(transform.position, currentWeapon.weaponReach);
+        Collider2D[] collidersInRange = Physics2D.OverlapCircleAll(transform.position, currentWeapon.weaponReach);
+        if (collidersInRange.Length > 0)
+        {
+            Debug.Log("Someone in range");
+            Debug.Log("Num of people: " + collidersInRange.Length);
+            Debug.Log(collidersInRange[0].name);
+        }
+        else
+        {
+            Debug.Log("no one in range");
+        }
 
         //goes through all objects in range of player
-        for(int i = 0; i < collidersInRange.Length; i++)
+        for (int i = 0; i < collidersInRange.Length; i++)
         {
             //Calculate the vector from the player to the collider in question
             Vector2 vectorToCollider = (collidersInRange[i].transform.position - transform.position);
@@ -32,6 +43,7 @@ public class PlayerAttack : MonoBehaviour
             //if the object is in front of player 
             if (Vector2.Dot(vectorToCollider, transform.forward) > 0)
             {
+                Debug.Log("Deal Damage");
                 //damage bad guy
             }
         }      
