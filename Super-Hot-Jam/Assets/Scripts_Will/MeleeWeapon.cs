37f152f493ca8595c4312323673eb61b031f39e7
Unknown <william4.james@live.uwe.ac.uk>
2020-10-22 16:08:49 +0100

    Basic MeleeV1
    Maybe works in theory still got a bit to do though

---
+++ Super-Hot-Jam/Assets/Scripts_Will/MeleeWeapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Melee Weapon", menuName = "Item/Melee")]
+public class MeleeWeapon : ScriptableObject
+{
+    public string weaponName = "Weapon Name";
+    public float weaponReach = 0.3f;
+    public float arcDistance = 0.3f;
+    public float attackSpeed = 1.0f;
+}
---
+++ Super-Hot-Jam/Assets/Scripts_Will/PlayerAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public MeleeWeapon currentWeapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+        //get all colliders around player within range of the weapon reach
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, currentWeapon.weaponReach);
+
+        //goes through all objects in range of player
+        for(int i = 0; i < collidersInRange.Length; i++)
+        {
+            //Calculate the vector from the player to the collider in question
+            Vector2 vectorToCollider = (collidersInRange[i].transform.position - transform.position);
+            vectorToCollider = vectorToCollider.normalized;
+
+            //if the object is in front of player 
+            if (Vector2.Dot(vectorToCollider, transform.forward) > 0)
+            {
+                //damage bad guy
+            }
+        }      
+    }
+}
