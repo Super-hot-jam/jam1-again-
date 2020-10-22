9968027c6c90ac29524c99d85af44d016c4bc2b3
Joseph <josephgarrad2017@gmail.com>
2020-10-22 13:57:56 +0100

    looking towards movmenet direction

--- Super-Hot-Jam/Assets/PlayerMovement.cs
+++ Super-Hot-Jam/Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D Rb;
     public float speed = 4;
     private Vector3 origpos;
+    float hori;
+    float verti;
+    Vector2 Look;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +21,14 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
  
-    void Movement()
+   public void Movement()
     {
 
 
 
-        Vector2 moveinput = new Vector2(Input.GetAxis("left_joyhori"), Input.GetAxis("left_joyverti"));
+       Vector2 moveinput = new Vector2(Input.GetAxis("left_joyhori"), Input.GetAxis("left_joyverti"));
+
+
 
         Vector2 forward = transform.up;
         Vector2 right = transform.right;
@@ -35,18 +41,29 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 DesMoveDir = (forward * -moveinput.y + right * moveinput.x).normalized;
 
-        Rb.MovePosition( transform.position + DesMoveDir * (speed * Time.deltaTime));
+        Rb.MovePosition(transform.position + DesMoveDir * (speed * Time.deltaTime));
 
-      
+        if (DesMoveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, DesMoveDir);
+        }
     }
-    void Update()
+   void Update()
     {
-        Movement();
-        //Vector2 Look = gameObject.GetComponent<Rigidbody2D>().velocity;
-        //if (Look != Vector2.zero)
-        //{
-        //    float angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg;
-        //    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        //}
+        hori = Input.GetAxis("left_joyhori");
+        verti = Input.GetAxis("left_joyverti");
+        Look = new Vector2(hori * speed, -verti * speed);
     }
+    private void FixedUpdate()
+    {
+
+        //Movement();
+        Rb.velocity = Look;
+        if (Look != Vector2.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, Look);
+        }
+
+    }
+  
 }
