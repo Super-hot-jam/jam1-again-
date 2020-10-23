4bb9d97999b0e7bbf667f88bbb4a77bf9f6822c6
HJNewton <43638999+HJNewton@users.noreply.github.com>
2020-10-23 16:32:41 +0100

    Main Audio track slows in game

--- Super-Hot-Jam/Assets/Scripts/TimeManager.cs
+++ Super-Hot-Jam/Assets/Scripts/TimeManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TimeManager : MonoBehaviour
 {
     public float slowdownFactor = 0.05f;
+    public AudioSource audio;
 
     void Update()
     {
@@ -15,11 +16,13 @@ public class TimeManager : MonoBehaviour
     {
         Time.timeScale = slowdownFactor;
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        audio.pitch = Mathf.Lerp(1, 0.35f,25 * Time.timeScale);
     }
 
     public void SpeedupTime()
     {
         Time.timeScale += (100)* Time.unscaledDeltaTime;
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        audio.pitch = Mathf.Lerp(0.35f, 1, 25 * Time.timeScale);
     }
 }
