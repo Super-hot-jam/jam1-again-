95096a16eeef5aed5a2086fc1a4137222f6850d5
Samsei <samuel3.carr@live.uwe.ac.uk>
2020-10-27 21:59:05 +0000

    Refactoring code

--- Super-Hot-Jam/Assets/AudioController.cs
+++ Super-Hot-Jam/Assets/AudioController.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    private AudioSource pistol;
-    private AudioSource shotgun;
+    private AudioSource main;
     private AudioSource menu;
-    private AudioSource music;
-    private AudioSource melee;
-    private AudioSource enemyDeath;
-    private AudioSource playerDeath;
 
     public AudioClip pistolClip;
     public AudioClip shotgunClip;
@@ -32,23 +27,13 @@ public class AudioController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pistol = GetComponent<AudioSource>();
-        shotgun = GetComponent<AudioSource>();
-        menu = GetComponent<AudioSource>();
-        music = GetComponent<AudioSource>();
-        melee = GetComponent<AudioSource>();
-        enemyDeath = GetComponent<AudioSource>();
-        playerDeath = GetComponent<AudioSource>();
+        main = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        pistol.pitch = Time.timeScale;
-        shotgun.pitch = Time.timeScale;
-        music.pitch = Time.timeScale;
-        melee.pitch = Time.timeScale;
-        enemyDeath.pitch = Time.timeScale;
+        main.pitch = Time.timeScale;
 
         while (inMenu)
         {
@@ -57,36 +42,36 @@ public class AudioController : MonoBehaviour
 
         while (inGame)
         {
-            music.Play();
+            main.Play();
         }
 
         if (pistolPlay)
         {
-            pistol.PlayOneShot(pistolClip, 0.5f);//Play pistol sound after firing
+            main.PlayOneShot(pistolClip, 0.5f);//Play pistol sound after firing
             pistolPlay = false;
         }
 
         if (shotgunPlay)
         {
-            shotgun.PlayOneShot(shotgunClip, 0.5f); //Play shotgun sound after firing
+            main.PlayOneShot(shotgunClip, 0.5f); //Play shotgun sound after firing
             shotgunPlay = false;
         }
 
         if (meleeHit)
         {
-            melee.PlayOneShot(meleeClip, 0.5f); //Play shotgun sound after firing
+            main.PlayOneShot(meleeClip, 0.5f); //Play shotgun sound after firing
             meleeHit = false;
         }
 
         if (enemyKill)
         {
-            enemyDeath.PlayOneShot(enemyDeathClip, 0.5f); //Play shotgun sound after firing
+            main.PlayOneShot(enemyDeathClip, 0.5f); //Play shotgun sound after firing
             enemyKill = false;
         }
 
         if (playerKill)
         {
-            playerDeath.PlayOneShot(playerDeathClip, 0.5f); //Play shotgun sound after firing
+            main.PlayOneShot(playerDeathClip, 0.5f); //Play shotgun sound after firing
             playerKill = false;
         }
     }
